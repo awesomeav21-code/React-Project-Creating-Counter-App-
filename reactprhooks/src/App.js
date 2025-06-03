484e6a3ecd5d6f863729250959a4b0e7ff5c788f
Avnish Ozarkar <aozarkar@charlotte.edu>
2025-06-03 11:23:40 -0400

    git commit -m "created some code in App.js" that builds a page with the title counter app.

--- reactprhooks/src/App.js
+++ reactprhooks/src/App.js
@@ -1,23 +1,10 @@
+import React, { Component } from 'react'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+class App extends Component {
+  render() {
+    return(
+      <h2>Counter app</h2>
+    ); 
+  } 
+} 
+export default App; 
