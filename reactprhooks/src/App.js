fd52660f2cf3ec7d4a5bdc9d9cac778d72d08580
Avnish Ozarkar <aozarkar@charlotte.edu>
2025-06-03 11:35:56 -0400

    git commit -m "created more code in the App.js" using useState and got rid of the this.increment and only wrote increment, made an increment function to add everytime the button is clicked.

--- reactprhooks/src/App.js
+++ reactprhooks/src/App.js
@@ -1,23 +1,46 @@
-import React, { Component } from 'react'; 
+import React, { useState } from 'react';
 
-class App extends Component {
-  state = {
-    count: 0 
-  }
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1 
-    })
-  }
-  render() {
-    return(
-      <div>
-        <h2>Counter App</h2>
-        <button onClick={this.increment}>Clicked {this.state.count} times
-        </button>
-      </div>
+const App = () => {
+  const [count, setCount] = useState(0);
 
-    ); 
-  } 
-} 
-export default App; 
+  // Define the increment function properly
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h2>Counter App</h2>
+      {/* Correct way to comment in JSX */}
+      <button onClick={increment}>
+        Clicked {count} times
+      </button>
+    </div>
+  );
+};
+
+export default App;
+
+
+
+// class App extends Component {
+//this is a functional component so you can't use state like so 
+//   state = {
+//     count: 0 
+//   }
+//   increment = () => {
+//     this.setState({
+//       count: this.state.count + 1 
+//     })
+//   }
+//   render() {
+//     return(
+//       <div>
+//         <h2>Counter App</h2>
+//         <button onClick={this.increment}>Clicked {this.state.count} times
+//         </button>
+//       </div>
+
+//     ); 
+//   } 
+// } 
